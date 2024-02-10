119b1d1ea5d8ebfb1dd71fab7e99ed3e77d21f51
Keith McDade <clydemcdade@MacBook-Pro.lan>
2024-02-10 13:33:56 -0500

    added functinoality for counting image requests and returning percentage of hits. added functionality for determining most used browser.

--- assignment3.py
+++ assignment3.py
@@ -1,94 +1,113 @@
 import argparse
 import csv
 import datetime
+# from datetime import datetime, timedelta
+from datetime import time
 import logging
 import shutil
 import tempfile
 import urllib.request
+import re
+import webbrowser
 from pprint import pprint
+import collections
+from itertools import groupby
 
 
-# this function will download a csv file and store it in a temp location
 def download_data(url):
-    # take url from argparse in main function and open it
     with urllib.request.urlopen(url) as response:
-        # save file in temp location and return value to caller
         with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
             shutil.copyfileobj(response, tmp_file)
             return tmp_file.name
 
 
-# this function processes the data in csv file from url
+class WebData:
+    def __init__(self, path, time, uagent, status, size):
+        self.path = path
+        self.time = time
+        self.uagent = uagent
+        self.status = status
+        self.size = size
+
+
 def process_data(file_contents):
-    # initialize dictionary to store contents of csv file
-    person_data = {}
-    # open file and parse with DictReader function
+    website_data = []
     with open(file_contents, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
+        reader = csv.reader(csvfile)
         for row in reader:
-            try:
-                # iterate through csv file to create a dictionary with ID# as key,
-                # with name and birthday as value stored in tuple,
-                # and with birthday converted to date object
-                person_data[int(row["id"])] = (
-                    row.get("name"), datetime.datetime.strptime(row.get("birthday"), '%d/%m/%Y').date())
-            # catches entries with problems in the birthday column
-            # and sends them to error.log via assignment2 function
-            except ValueError:
-                # ID# and row name are saved to variables here, so they can be passed
-                # to assignment2 func and properly added to error log
-                id_num = row["id"]
-                name = row["name"]
-                assignment_2(id_num, name)
-                continue
-        return person_data
+            # web = WebData(row[0], row[1], row[2], row[3], row[4])
+            website_data.append(row[0:3:1])
+        # pprint(website_data)
+        # print(web.uagent)
+        return website_data
 
 
-# this function displays name and birthday associated with
-# values from csv file from input from user_input function
-# 1. catch input that is above 0 but is not associated with ID# in csv file
-# 2. find name and birthday associated with ID# and print
-# 3. exit program if and number <=0 is entered by user
-def display_person(input_id, person_data):
-    while True:
-        if input_id > 0 and input_id not in person_data:
-            print('ID number does not match database.')
-            input_id = int(input('Please input an ID number: \n'))
-        elif input_id in person_data:
-            id_info = person_data[input_id]
-            (name, birthday) = id_info
-            # pprint(person_data) # uncomment to see full dictionary from csv file
-            final_output = ('Person {} is {} with a birthday of {}.\n'.format(input_id, name, birthday))
-            break
-        elif input_id <= 0:
-            print('ID number must be at least 1.\n')
-            exit()
-    return final_output
+def search_hits(website_data):
+    total_hits = len(website_data)
+    img = 0
+    for i in website_data[:]:
+        current_row = website_data.pop(0)
+        img_hits = current_row[0]
+        website_data.extend([current_row[1:]])
+        if re.findall(r'([^\s]+(?=\.(jpg|gif|png))\.\2)', img_hits.lower()):
+            img += 1
+        else:
+            continue
+    img_hits = 100 * float(img / total_hits)
+    return img_hits
 
 
-# this function logs errors from process_data function and logs it to error.log
-def assignment_2(id_num, name):
-    id_err = id_num
-    name_err = name
+def get_browser(website_data):
+    browser_ls = []
+    # browser_set = []
+    chrome = re.compile(r'Chrome/')
+    firefox = re.compile(r'Firefox/')
+    safari = re.compile(r'Version/')
+    ie = re.compile(r'Trident/')
+    count_chr = 0
+    count_ff = 0
+    count_saf = 0
+    count_ie = 0
+    for i in website_data[:]:
+        current_row = website_data.pop(0)
+        browser_hits = current_row.pop()
+        browser_ls.append(browser_hits)
+        # browser_set = set(browser_ls)
+        website_data.extend(current_row)
+        if chrome.findall(browser_hits):
+            count_chr += 1
+        elif firefox.findall(browser_hits):
+            count_ff += 1
+        elif safari.findall(browser_hits):
+            count_saf += 1
+        elif ie.findall(browser_hits):
+            count_ie += 1
+        else:
+            continue
+    browser_count = {'Chrome': count_chr, 'Firefox': count_ff, 'Safari': count_saf, 'Internet Explorer': count_ie}
+    browser_max = max(browser_count, key=browser_count.get)
+    # pprint(website_data[::-1])
+    return browser_max
 
+
+# def get_time(website_data):
+#     dates_list = [datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S') for date in website_data]
+#     start = datetime.datetime.fromisoformat('2014-01-27 00:00:00')
+#     end = datetime.datetime.fromisoformat('2014-01-27 23:59:59')
+#     res_time = start
+#     for i in dates_list[:]:
+#         hit = dates_list.pop(0)
+#         while hit <= end:
+#             hit += datetime.timedelta(seconds=1)
+
+
+def assignment_2():
     logging.basicConfig(filename='error.log', filemode='w')
-    logging.error('“Error processing line {} for ID {}”'.format(id_err, name_err))
-
-
-def user_input():
-    # take user input
-    input_id = input('Please input an ID number: \n')
-    try:
-        # check if input is valid data type, in this case int
-        id_number = int(input_id)
-    except ValueError:
-        # directs user to attempt input again if an invalid value is entered
-        id_number = int(input("Sorry, that didn't work.\nPlease input an ID number: \n"))
-    return id_number
+    # logging.error('“Error processing line {} for ID {}”'.format(id_err, name_err))
 
 
 def main(url):
-    print(f"Running main with URL = {url}...\n")
+    print(f"Running main with URL = {url}...")
 
 
 if __name__ == "__main__":
@@ -97,10 +116,13 @@ if __name__ == "__main__":
     parser.add_argument('--url', help='url to download', type=str, required=True,)
     args = parser.parse_args()
     main(args.url)
-    # program functions called here,
-    # output from display_person function prints here
-    user_input = user_input()
-    csv_output = download_data(args.url)
-    process_output = process_data(csv_output)
-    display_output = display_person(user_input, process_output)
-    print(display_output)
+
+    csv_file = download_data(args.url)
+    process_output = process_data(csv_file)
+
+    hits = search_hits(process_output)
+    print(f"Image requests account for {hits}% of all requests.")
+    browser = get_browser(process_output)
+    print(f"{browser} was the most used browser for 1/27/14.")
+    # time = get_time(process_output)
+    # pprint(time)
